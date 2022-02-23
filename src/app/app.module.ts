b0cc6a90f987f051b910198f201ea03db3ae777d
Jorge Coronel González <tprog.jorge.coronel@outlook.com>
2022-02-23 10:43:50 -0600

    Spinner * Se agrega spinner junto con un interceptor y service para mostrarlo antes de cada petición y después de que se concluya la petición

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from "./core/core.module";
+import { SpinnerInterceptor } from "./core/interceptors/spinner.interceptor";
 
 @NgModule({
   declarations: [
@@ -12,9 +15,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    CoreModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: SpinnerInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/core/components/spinner/spinner.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { Subject } from "rxjs";
+import { SpinnerService } from "../../services/spinner.service";
+
+@Component({
+  selector: 'app-spinner',
+  template: `
+    <div class="overlay" *ngIf="isLoading$ | async">
+      <mat-progress-spinner [color]="'accent'"
+                            [mode]="'indeterminate'">
+      </mat-progress-spinner>
+    </div>
+  `,
+  styles: [`
+    .overlay {
+      align-items: center;
+      background-color: rgba(0,0,0,.2);
+      display: flex;
+      height: 100%;
+      justify-content: center;
+      position: fixed;
+      width: 100%;
+      z-index: 10;
+    }
+  `]
+})
+export class SpinnerComponent {
+  public isLoading$: Subject<boolean>;
+
+  constructor(private spinnerService: SpinnerService) {
+    this.isLoading$ = this.spinnerService.isLoading$;
+  }
+
+}
