1a87c79b8e85bbab3156a61a9819010e478fc723
Jorge Coronel González <tprog.jorge.coronel@outlook.com>
2022-02-24 09:30:30 -0600

    Se agrega mat divider al material.module.ts

--- src/app/core/material/material.module.ts
+++ src/app/core/material/material.module.ts
@@ -2,6 +2,7 @@ import { LayoutModule } from "@angular/cdk/layout";
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
+import { MatDividerModule } from "@angular/material/divider";
 import { MatIconModule } from "@angular/material/icon";
 import { MatListModule } from "@angular/material/list";
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
@@ -13,6 +14,7 @@ import { MatToolbarModule } from "@angular/material/toolbar";
     LayoutModule,
     MatButtonModule,
     MatCardModule,
+    MatDividerModule,
     MatIconModule,
     MatListModule,
     MatProgressSpinnerModule,
