846719ba80ff5adeea3fe60a3ef0f4cf7513ff52
Jorge Coronel González <tprog.jorge.coronel@outlook.com>
2022-02-24 09:32:34 -0600

    Se agrega método al servicio book-portal.service.ts para buscar un libro por id

--- src/app/portal/services/book-portal.service.ts
+++ src/app/portal/services/book-portal.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable, tap, combineLatestAll, scheduled, asyncScheduler } from "rxjs";
 import { map } from "rxjs/operators";
 import { ListResponse } from "../../core/models/list-response";
+import { SingleResponse } from "../../core/models/single-response";
 import { EnvironmentService } from "../../core/services/environment.service";
 import { BookPortalModel } from "../models/book-portal.model";
 
@@ -10,25 +11,36 @@ import { BookPortalModel } from "../models/book-portal.model";
   providedIn: 'root'
 })
 export class BookPortalService {
+  private _baseUrl: string = '/portal/books';
+
   constructor(private environmentService: EnvironmentService,
               private http: HttpClient) {}
 
   public findLatest(): Observable<BookPortalModel[]> {
-    const url = `${this.environmentService.environmentApi}/portal/books/latest`;
+    const url = `${this.environmentService.environmentApi}${this._baseUrl}/latest`;
     return this.http.get<ListResponse<BookPortalModel>>(url)
       .pipe(
         map(res => res.data!),
-        tap(this.getAuthorStrCard)
+        tap(books => books.forEach(this.getAuthorStrCard))
       )
   }
 
   public findMostRead(): Observable<BookPortalModel[]> {
-    const url = `${this.environmentService.environmentApi}/portal/books/most-read`;
+    const url = `${this.environmentService.environmentApi}${this._baseUrl}/most-read`;
     return this.http.get<ListResponse<BookPortalModel>>(url)
       .pipe(
         map(res => res.data!),
-        tap(this.getAuthorStrCard)
+        tap(books => books.forEach(this.getAuthorStrCard))
       )
+  }
+
+  public findById(id: number): Observable<BookPortalModel> {
+    const url = `${this.environmentService.environmentApi}${this._baseUrl}/detail/${id}`;
+    return this.http.get<SingleResponse<BookPortalModel>>(url)
+      .pipe(
+        map(res => res.data!),
+        tap(this.getAuthorStrCard)
+      );
   }
 
   public findAllIndex(): Observable<BookPortalModel[][]> {
@@ -38,18 +50,18 @@ export class BookPortalService {
       );
   }
 
-  private getAuthorStrCard(books: BookPortalModel[]): void {
-    books.forEach(book => {
-      let authorArray: string[] = [];
-      book.authors.forEach(author => {
-        const authorSplit = author.name.split(' ');
-        if (authorSplit.length >= 2) {
-          authorArray.push(`${authorSplit[0]} ${authorSplit[1]}`);
-        } else {
-          authorArray.push(authorSplit[1]);
-        }
-      });
-      book.authorsStr = authorArray.join(', ');
+  private getAuthorStrCard(book: BookPortalModel): void {
+    let authorArray: string[] = [];
+
+    book.authors.forEach(author => {
+      const authorSplit = author.name.split(' ');
+      if (authorSplit.length >= 2) {
+        authorArray.push(`${authorSplit[0]} ${authorSplit[1]}`);
+      } else {
+        authorArray.push(authorSplit[1]);
+      }
     });
+
+    book.authorsStr = authorArray.join(', ');
   }
 }
