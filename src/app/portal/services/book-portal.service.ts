9a6152f38c8b55521745c5cc5b6748ea9d404d82
Jorge Coronel González <tprog.jorge.coronel@outlook.com>
2022-03-01 10:15:12 -0600

    Servicio de libros * Se agrega método findAll para obtener el listado de libros * Se agrega método getMinMaxPages para obtener el min y max de páginas de los libros * Se combinan peticiones con forkjoin

--- src/app/portal/services/book-portal.service.ts
+++ src/app/portal/services/book-portal.service.ts
@@ -1,11 +1,13 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from '@angular/core';
-import { Observable, tap, combineLatestAll, scheduled, asyncScheduler } from "rxjs";
+import { Observable, tap, forkJoin } from "rxjs";
 import { map } from "rxjs/operators";
 import { ListResponse } from "../../core/models/list-response";
 import { SingleResponse } from "../../core/models/single-response";
 import { EnvironmentService } from "../../core/services/environment.service";
-import { BookPortalModel } from "../models/book-portal.model";
+import { BookPortalModel, MinMaxPagesModel } from "../models/book-portal.model";
+import { LiteraryGenderModel } from "../models/literary-gender.model";
+import { LiteraryGenderService } from "./literary-gender.service";
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +16,22 @@ export class BookPortalService {
   private _baseUrl: string = '/portal/books';
 
   constructor(private environmentService: EnvironmentService,
+              private literaryGenderService: LiteraryGenderService,
               private http: HttpClient) {}
+
+  public findAll(sort: string, itemsPerPage: number = 12, page: number = 1, search: string|null = null): Observable<ListResponse<BookPortalModel>> {
+    const params = new HttpParams()
+      .append('sort', sort)
+      .append('per_page', itemsPerPage)
+      .append('page', page)
+      .append('q', (search === null) ? '' : encodeURI(search));
+
+    const url = `${this.environmentService.environmentApi}${this._baseUrl}`;
+    return this.http.get<ListResponse<BookPortalModel>>(url, { params })
+      .pipe(
+        tap(res => res.data!.forEach(this.getAuthorStrCard))
+      );
+  }
 
   public findLatest(): Observable<BookPortalModel[]> {
     const url = `${this.environmentService.environmentApi}${this._baseUrl}/latest`;
@@ -43,11 +60,20 @@ export class BookPortalService {
       );
   }
 
-  public findAllIndex(): Observable<BookPortalModel[][]> {
-    return scheduled([this.findLatest(), this.findMostRead()], asyncScheduler)
+  public getMinMaxPages(): Observable<MinMaxPagesModel> {
+    const url = `${this.environmentService.environmentApi}${this._baseUrl}/min-max-pages`;
+    return this.http.get<SingleResponse<MinMaxPagesModel>>(url)
       .pipe(
-        combineLatestAll()
+        map(res => res.data!)
       );
+  }
+
+  public findAllIndex(): Observable<BookPortalModel[][]> {
+    return forkJoin([this.findLatest(), this.findMostRead()]);
+  }
+
+  public combineRequestListBook(sort: string): Observable<[ListResponse<BookPortalModel>, MinMaxPagesModel, LiteraryGenderModel[]]> {
+    return forkJoin([this.findAll(sort), this.getMinMaxPages(), this.literaryGenderService.findAll()]);
   }
 
   private getAuthorStrCard(book: BookPortalModel): void {
