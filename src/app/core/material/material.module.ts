597651cb15565d71d1219d0ec6f03307d041b560
Jorge Coronel González <tprog.jorge.coronel@outlook.com>
2022-03-01 10:06:02 -0600

    Material module * Se agregan los siguientes componentes: - MatCheckbox - MatExpansion - MatInput - MatPaginator - MatSlider

--- src/app/core/material/material.module.ts
+++ src/app/core/material/material.module.ts
@@ -2,11 +2,17 @@ import { LayoutModule } from "@angular/cdk/layout";
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
+import { MatCheckboxModule } from "@angular/material/checkbox";
 import { MatDividerModule } from "@angular/material/divider";
+import { MatExpansionModule } from "@angular/material/expansion";
 import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
 import { MatListModule } from "@angular/material/list";
+import { MatPaginatorModule } from "@angular/material/paginator";
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatSelectModule } from "@angular/material/select";
 import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatSliderModule } from "@angular/material/slider";
 import { MatToolbarModule } from "@angular/material/toolbar";
 
 @NgModule({
@@ -14,12 +20,18 @@ import { MatToolbarModule } from "@angular/material/toolbar";
     LayoutModule,
     MatButtonModule,
     MatCardModule,
+    MatCheckboxModule,
     MatDividerModule,
+    MatExpansionModule,
     MatIconModule,
+    MatInputModule,
     MatListModule,
+    MatPaginatorModule,
     MatProgressSpinnerModule,
-    MatToolbarModule,
-    MatSidenavModule
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatToolbarModule
   ]
 })
 export class MaterialModule { }
