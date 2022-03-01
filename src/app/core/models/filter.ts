742ecfa80c30312c27e910263074aab294a78855
Jorge Coronel González <tprog.jorge.coronel@outlook.com>
2022-03-01 09:51:55 -0600

    Modelos para filtros

---
+++ src/app/core/models/filter.ts
@@ -0,0 +1,14 @@
+export interface Filter {
+  field: string;
+  type: TypesEnum;
+  value: any
+}
+
+export enum TypesEnum {
+  Array = 'array',
+  Boolean = 'boolean',
+  Date = 'date',
+  Double = 'double',
+  Int = 'int',
+  String = 'string'
+}
