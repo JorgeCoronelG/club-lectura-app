0ab65d4f169686750b06b444975eae6d77c62e08
Jorge Coronel González <tprog.jorge.coronel@outlook.com>
2022-03-03 17:57:53 -0600

    * Se crea auth.guard.ts para verificar si está autenticado y sino, lo regresa al login * en app-routing.module.ts se agregan las rutas del auth y del dashboard, se agrega el guard de auth.guard.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from "./core/guards/auth.guard";
 
 const routes: Routes = [
   {
     path: 'portal',
     loadChildren: () => import('./portal/portal.module').then(m => m.PortalModule)
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule),
+    canActivate: [ AuthGuard ],
+    canLoad: [ AuthGuard ]
   },
   {
     path: '**',
---
+++ src/app/core/guards/auth.guard.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanLoad,
+  Route,
+  RouterStateSnapshot,
+  UrlSegment,
+  Router
+} from '@angular/router';
+import { Observable, catchError, of, tap } from 'rxjs';
+import { map } from "rxjs/operators";
+import { UserSessionService } from "../services/user-session.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate, CanLoad {
+  constructor(private userSessionService: UserSessionService,
+              private router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot,state: RouterStateSnapshot): Observable<boolean> {
+    return this.isAuthenticated();
+  }
+
+  canLoad(route: Route,segments: UrlSegment[]): Observable<boolean> {
+    return this.isAuthenticated();
+  }
+
+  private isAuthenticated(): Observable<boolean> {
+    return this.userSessionService.getUserSession()
+      .pipe(
+        map(() => true),
+        catchError(() => of(false)),
+        tap((isAuthenticated) => {
+          if (!isAuthenticated) {
+            this.userSessionService.removeToken();
+            this.router.navigateByUrl('/auth/login');
+          }
+        })
+      );
+  }
+}
