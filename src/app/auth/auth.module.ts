39c4eca0fd4f5b605c011d2d17e80d92ca475213
Jorge Coronel González <tprog.jorge.coronel@outlook.com>
2022-03-03 18:13:48 -0600

    Módulo login * Se crea auth.module.ts para el módulo de auth * Se crea auth-routing.module.ts para la ruta del login

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { ReactiveFormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { MaterialModule } from "../material/material.module";
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './pages/login/login.component';
+
+
+@NgModule({
+  declarations: [
+    LoginComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    MaterialModule,
+    AuthRoutingModule,
+  ]
+})
+export class AuthModule { }
