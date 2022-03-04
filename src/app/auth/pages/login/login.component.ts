744e17b9b4fd02cc3f9b53ddb61f629c8439f36f
Jorge Coronel González <tprog.jorge.coronel@outlook.com>
2022-03-03 18:12:05 -0600

    Se crea componente de la pantalla de login

---
+++ src/app/auth/pages/login/login.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { FormErrors } from "../../../core/utils/form-errors";
+import { UserModel } from "../../models/user.model";
+import { AuthService } from "../../services/auth.service";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  public loginForm: FormGroup;
+  public formErrors: FormErrors;
+
+  constructor(private formBuilder: FormBuilder,
+              private router: Router,
+              private authService: AuthService) {
+    this.loginForm = this.formBuilder.group({
+      email: [localStorage.getItem('email-session') || '',
+        [Validators.required, Validators.email, Validators.maxLength(120)]],
+      password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(50)]],
+      rememberMe: [(localStorage.getItem('email-session') !== null)]
+    });
+
+    this.formErrors = new FormErrors(this.loginForm);
+  }
+
+  ngOnInit(): void {}
+
+  public login(): void {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    const user: UserModel = this.loginForm.value;
+    this.authService.login(user).subscribe(() => {
+      if (user.rememberMe) {
+        localStorage.setItem('email-session', user.email!);
+      } else {
+        localStorage.removeItem('email-session');
+      }
+
+      this.router.navigateByUrl('/dashboard');
+    });
+  }
+}
