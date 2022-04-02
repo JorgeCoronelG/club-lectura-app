7afb3dc1efa3e4ebaa674898f15d90dcdc56f656
JorgeCoronelG <tprog.jorge.coronel@outlook.com>
2022-04-02 11:45:54 -0600

    Modelo de filtros * Se elimina modelo filter.ts y se dejan todas las interfaces en el archivo de filters.ts

--- src/app/core/models/filters.ts
+++ src/app/core/models/filters.ts
@@ -1,5 +1,18 @@
-import { Filter } from "./filter";
-
 export interface Filters {
   filters: Filter[];
+}
+
+export interface Filter {
+  field: string;
+  type: TypesEnum;
+  value: any
+}
+
+export enum TypesEnum {
+  Array = 'array',
+  Boolean = 'boolean',
+  Date = 'date',
+  Double = 'double',
+  Int = 'int',
+  String = 'string'
 }
