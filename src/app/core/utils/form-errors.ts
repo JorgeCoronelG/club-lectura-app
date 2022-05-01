9cc3063d6cc9252b0cf99b45417aa7239c8823a2
JorgeCoronelG <tprog.jorge.coronel@outlook.com>
2022-05-01 11:02:22 -0500

    form-errors.ts * Se quitan métodos y se dejan genéricos

--- src/app/core/utils/form-errors.ts
+++ src/app/core/utils/form-errors.ts
@@ -11,31 +11,15 @@ export class FormErrors {
     return this.form.get(field)?.hasError(error) && this.form.get(field)?.touched;
   }
 
-  public email(field: string): boolean | undefined {
-    return this.form.get(field)?.hasError('email') && this.form.get(field)?.touched;
-  }
-
   public errorObject(field: string, error: string): any {
     return this.form.get(field)?.getError(error);
-  }
-
-  public maxLength(field: string): boolean | undefined {
-    return this.form.get(field)?.hasError('maxlength') && this.form.get(field)?.touched;
   }
 
   public maxLengthNumber(field: string): number {
     return this.errorObject(field, 'maxlength').requiredLength;
   }
 
-  public minLength(field: string): boolean | undefined {
-    return this.form.get(field)?.hasError('minlength') && this.form.get(field)?.touched;
-  }
-
   public minLengthNumber(field: string): number {
     return this.errorObject(field, 'minlength').requiredLength;
-  }
-
-  public required(field: string): boolean | undefined {
-    return this.form.get(field)?.hasError('required') && this.form.get(field)?.touched;
   }
 }
