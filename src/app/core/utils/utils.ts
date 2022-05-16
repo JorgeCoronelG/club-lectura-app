671255e540609c4f05389c9c5363f7655a6df48f
JorgeCoronelG <tprog.jorge.coronel@outlook.com>
2022-05-15 19:12:02 -0500

    Utils * Se agrega constante para los formatos de fecha para el datepicker

--- src/app/core/utils/utils.ts
+++ src/app/core/utils/utils.ts
@@ -1,3 +1,15 @@
 export const convertCamelCaseToSnakeCase = (str: string): string => {
   return str.replace(/[A-Z]/g, letter => `_${letter.toLowerCase()}`);
 }
+
+export const DATE_FORMATS_DATEPICKER = {
+  parse: {
+    dateInput: 'DD/MM/YYYY',
+  },
+  display: {
+    dateInput: 'DD/MM/YYYY',
+    monthYearLabel: 'MMMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY'
+  },
+}
