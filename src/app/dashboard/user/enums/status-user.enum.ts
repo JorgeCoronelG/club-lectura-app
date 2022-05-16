645cf13e284e6ce1d2c52df69e2d9d0556cc2e25
JorgeCoronelG <tprog.jorge.coronel@outlook.com>
2022-05-15 19:27:12 -0500

    status-user.enum.ts * Se agrega opciones para los textos de el estatus del usuario

--- src/app/dashboard/user/enums/status-user.enum.ts
+++ src/app/dashboard/user/enums/status-user.enum.ts
@@ -1,5 +1,8 @@
 export enum StatusUserEnum {
   Active = 1,
   Inactive = 2,
-  Blocked = 3 // En deuda
+  Blocked = 3, // En deuda
+  ActiveStr = 'Activo',
+  InactiveStr = 'Inactivo',
+  BlockedStr = 'En deuda'
 }
