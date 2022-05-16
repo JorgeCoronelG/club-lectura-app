8bbbf973a0dc4a77ec5eeffee3debc801320fbb2
JorgeCoronelG <tprog.jorge.coronel@outlook.com>
2022-05-15 19:20:26 -0500

    Usuario service * Se agrega método para transformar la información que viene del back, en este caso, convertir los estatus de usuarios a strings

--- src/app/dashboard/user/services/user.service.ts
+++ src/app/dashboard/user/services/user.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from '@angular/core';
-import { Observable } from "rxjs";
+import { Observable, tap } from "rxjs";
 import { ListResponse } from "../../../core/models/list-response";
 import { EnvironmentService } from "../../../core/services/environment.service";
+import { StatusUserEnum } from "../enums/status-user.enum";
 import { getPaginateParams } from "../../../core/utils/http-functions";
 import { UserModel } from "../models/user.model";
 
@@ -22,6 +23,26 @@ export class UserService {
   public findAll(sort: string, itemsPerPage: number, page: number, search: string): Observable<ListResponse<UserModel>> {
     const params = getPaginateParams(sort, itemsPerPage, page, search);
 
-    return this.http.get<ListResponse<UserModel>>(this.url, { params });
+    return this.http.get<ListResponse<UserModel>>(this.url, { params })
+      .pipe(
+        tap(res => {
+          res.data?.forEach(this.transformInfo);
+          return res;
+        })
+      );
+  }
+
+  public transformInfo(user: UserModel): void {
+    switch (user.status) {
+      case StatusUserEnum.Active:
+        user.statusStr = StatusUserEnum.ActiveStr;
+        break;
+      case StatusUserEnum.Inactive:
+        user.statusStr = StatusUserEnum.InactiveStr;
+        break;
+      case StatusUserEnum.Blocked:
+        user.statusStr = StatusUserEnum.BlockedStr;
+        break;
+    }
   }
 }
