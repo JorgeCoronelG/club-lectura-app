2cf190019cbabe6c8628ffba128bf2032241068e
JorgeCoronelG <tprog.jorge.coronel@outlook.com>
2022-05-15 19:18:25 -0500

    Role service * Se crea el servicio de rol * Se agrega método findAll para buscar todos los roles

---
+++ src/app/dashboard/user/services/role.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from '@angular/core';
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { ListResponse } from "../../../core/models/list-response";
+import { EnvironmentService } from "../../../core/services/environment.service";
+import { RoleModel } from "../models/role.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RoleService {
+  private _baseUrl = '/roles';
+
+  constructor(private environmentService: EnvironmentService,
+              private http: HttpClient) {}
+
+  get url(): string {
+    return `${this.environmentService.environmentApi}${this._baseUrl}`;
+  }
+
+  public findAll(): Observable<RoleModel[]> {
+    return this.http.get<ListResponse<RoleModel>>(`${this.url}/find-all`)
+      .pipe(
+        map(res => res.data!)
+      );
+  }
+}
