a4f7c86ed8c2e9d5398e1885e61dd3d7768576a3
JorgeCoronelG <tprog.jorge.coronel@outlook.com>
2022-06-11 01:14:35 -0500

    Refactor enums lenguaje libros * Se agrega enum para las traducciones del lenguaje * Se agrega código para agregar las traducciones del libro y las traducciones del estatus del libro

--- src/app/portal/enums/language-book.enum.ts
+++ src/app/portal/enums/language-book.enum.ts
@@ -2,3 +2,8 @@ export enum LanguageBookEnum {
   Spanish = 1,
   English = 2
 }
+
+export enum LanguageBookNameEnum {
+  Spanish = 'Español',
+  English = 'Inglés'
+}
--- src/app/portal/pages/book-detail/book-detail.component.ts
+++ src/app/portal/pages/book-detail/book-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from "@angular/router";
 import { I18nPlural } from "../../../core/models/i18n-plural";
+import { LanguageBookNameEnum, LanguageBookEnum } from "../../enums/language-book.enum";
+import { StatusBookEnum, StatusBookNameEnum } from "../../enums/status-book.enum";
 import { BookPortalModel } from "../../models/book-portal.model";
 import { BookPortalService } from "../../services/book-portal.service";
 
@@ -12,6 +14,8 @@ import { BookPortalService } from "../../services/book-portal.service";
 export class BookDetailComponent implements OnInit {
   public book?: BookPortalModel;
   public authorMessageMapping: I18nPlural = { '=1': 'Autor:', 'other': 'Autores:' };
+  public languageBook: string = '';
+  public _statusBook: string = '';
 
   constructor(private router: Router,
               private activatedRoute: ActivatedRoute,
@@ -21,13 +25,21 @@ export class BookDetailComponent implements OnInit {
     this.activatedRoute.params.subscribe(({ id }) => this.findBookById(id));
   }
 
+  get statusBook(): typeof StatusBookEnum {
+    return StatusBookEnum;
+  }
+
   public redirect(): void {
     this.router.navigateByUrl('/portal');
   }
 
   private findBookById(id: number): void {
     this.bookPortalService.findById(id).subscribe({
-      next: (book) => this.book = book,
+      next: (book) => {
+        this.book = book
+        this.languageBook = LanguageBookNameEnum[LanguageBookEnum[book.language!] as keyof typeof LanguageBookNameEnum];
+        this._statusBook = StatusBookNameEnum[StatusBookEnum[book.status] as keyof typeof StatusBookNameEnum];
+      },
       error: () => this.redirect()
     });
   }
