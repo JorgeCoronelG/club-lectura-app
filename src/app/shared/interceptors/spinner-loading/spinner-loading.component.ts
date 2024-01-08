0305c42cf00135329ffba80e511397e1f7444b22
Jorge Coronel <jorge.coronel@outlook.com>
2024-01-08 17:58:04 -0600

    spinner-loading.component.ts - Se cambia el pipe async por el change detection

--- src/app/shared/interceptors/spinner-loading/spinner-loading.component.ts
+++ src/app/shared/interceptors/spinner-loading/spinner-loading.component.ts
@@ -1,6 +1,5 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Observable } from 'rxjs';
 import { SpinnerLoadingService } from '@shared/interceptors/spinner-loading/spinner-loading.service';
 
 @Component({
@@ -8,7 +7,7 @@ import { SpinnerLoadingService } from '@shared/interceptors/spinner-loading/spin
   standalone: true,
   imports: [CommonModule],
   template: `
-    <div *ngIf="isLoading$ | async" class="overlay flex flex-col">
+    <div *ngIf="isLoading" class="overlay flex flex-col">
       <div class="loader book">
         <figure class="page"></figure>
         <figure class="page"></figure>
@@ -18,14 +17,23 @@ import { SpinnerLoadingService } from '@shared/interceptors/spinner-loading/spin
       <h1>CARGANDO</h1>
     </div>
   `,
-  styleUrls: ['./spinner-loading.component.scss']
+  styleUrls: ['./spinner-loading.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SpinnerLoadingComponent {
-  isLoading$: Observable<boolean>;
+  isLoading: boolean = false;
 
   spinnerLoadingService: SpinnerLoadingService = inject(SpinnerLoadingService);
 
-  constructor() {
-    this.isLoading$ = this.spinnerLoadingService.isLoading$.asObservable();
+  constructor(
+    private cd: ChangeDetectorRef
+  ) {
+    this.spinnerLoadingService.isLoading$.asObservable()
+      .subscribe((isLoading) => {
+        if (this.isLoading !== isLoading) {
+          this.isLoading = isLoading;
+          this.cd.detectChanges();
+        }
+      });
   }
 }
