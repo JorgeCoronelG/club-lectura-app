c8712c7d8a8ab80cbdfc840b4a0b4f502932b6c4
Jorge Coronel <jorge.coronel@outlook.com>
2024-01-16 13:51:19 -0600

    Redireccionar en caso de no tener el rol permitido en la ruta

--- src/app/shared/guards/permission-role.guard.ts
+++ src/app/shared/guards/permission-role.guard.ts
@@ -1,4 +1,4 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { UserSessionService } from '@shared/services/user-session.service';
 import { inject } from '@angular/core';
 
@@ -7,12 +7,18 @@ export function permissionRoleGuard(
 ): CanActivateFn {
   return (): boolean => {
     const userSessionService: UserSessionService = inject(UserSessionService);
+    const router: Router = inject(Router);
 
     if (!userSessionService.user) {
+      router.navigateByUrl('/dashboard');
       return false;
     }
 
     const authorize = rolesId.find(id => userSessionService.user.rolId === id);
+
+    if (authorize == undefined) {
+      router.navigateByUrl('/dashboard');
+    }
 
     return authorize != undefined;
   }
