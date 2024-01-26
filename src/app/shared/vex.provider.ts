46f68f4de5f4a005212f0fe7c8a581b0018bf23b
Jorge Coronel <jorge.coronel@outlook.com>
2024-01-26 11:48:14 -0600

    Refactor de componentes que no se usaban dentro del proyecto

--- src/app/shared/vex.provider.ts
+++ src/app/shared/vex.provider.ts
@@ -1,28 +1,17 @@
-import {
-  ENVIRONMENT_INITIALIZER,
-  EnvironmentProviders,
-  importProvidersFrom,
-  inject,
-  Provider
-} from '@angular/core';
-import {
-  MAT_FORM_FIELD_DEFAULT_OPTIONS,
-  MatFormFieldDefaultOptions
-} from '@angular/material/form-field';
+import { ENVIRONMENT_INITIALIZER, EnvironmentProviders, inject, Provider } from '@angular/core';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldDefaultOptions } from '@angular/material/form-field';
 import { MATERIAL_SANITY_CHECKS } from '@angular/material/core';
 import { VexLayoutService } from '@shared/services/vex-layout.service';
 import { VexDemoService } from '@shared/services/vex-demo.service';
 import { VexPlatformService } from '@shared/services/vex-platform.service';
 import { VexConfig, VexThemeProvider } from '@shared/config/vex-config.interface';
 import { VEX_CONFIG, VEX_THEMES } from '@shared/config/config.token';
-import { VexHighlightModule } from '@shared/components/vex-highlight/vex-highlight.module';
 
 export function provideVex(options: {
   config: VexConfig;
   availableThemes: VexThemeProvider[];
 }): (Provider | EnvironmentProviders)[] {
   return [
-    importProvidersFrom(VexHighlightModule),
     {
       provide: VEX_CONFIG,
       useValue: options.config
