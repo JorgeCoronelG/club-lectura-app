3ac7d58694422cb38bb1617894cc1b578afed527
Jorge Coronel <jorge.coronel@outlook.com>
2024-06-05 12:27:18 -0600

    Se agrega funcionalidad para agregar un usuario nuevo al sistema con el formulario

--- src/app/dashboard/donations/pages/donations-managment/donations-managment.component.ts
+++ src/app/dashboard/donations/pages/donations-managment/donations-managment.component.ts
@@ -11,7 +11,16 @@ import { NgFor, NgIf } from '@angular/common';
 import { UserService } from '@shared/services/user.service';
 import { Usuario } from '@shared/models/usuario.model';
 import { trackById } from '@shared/utils/track-by';
-import { uniqueUserValidator } from './validators-donation-form';
+import { generateValidatorsTypeUser, uniqueUserValidator } from './validators-donation-form';
+import { OnlyNumbersDirective } from '@shared/directives/only-numbers.directive';
+import { forkJoin } from 'rxjs';
+import { CatalogoOpcion } from '@shared/models/catalogo-opcion.model';
+import { Rol } from '@shared/models/role.model';
+import { CatalogoEnum } from '@shared/enums/catalogo.enum';
+import { OptionCatalogService } from '@shared/services/option-catalog.service';
+import { RolService } from '@shared/services/rol.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { TypeUserEnum } from '@shared/enums/catalogo-opciones/type-user.enum';
 
 @Component({
   selector: 'app-donations-managment',
@@ -24,6 +33,7 @@ import { uniqueUserValidator } from './validators-donation-form';
     ReactiveFormsModule,
     NgIf,
     NgFor,
+    OnlyNumbersDirective,
   ],
   templateUrl: './donations-managment.component.html',
   styles: [],
@@ -37,17 +47,27 @@ import { uniqueUserValidator } from './validators-donation-form';
 export class DonationsManagmentComponent implements OnInit {
   private readonly destroyRef: DestroyRef = inject(DestroyRef);
 
-  usersForm!: UntypedFormGroup;
-  users: Usuario[] = [];
-
   readonly trackById = trackById;
   readonly USER_BD = 1;
   readonly NEW_USER = 2;
+
+  usersForm!: UntypedFormGroup;
+  users: Usuario[] = [];
+  now: Date = new Date();
+  sexOptions: CatalogoOpcion[] = [];
+  rolesOptions: Rol[] = [];
+  typeEscolarOptions: CatalogoOpcion[] = [];
+  turnAlumnoOptions: CatalogoOpcion[] = [];
+  typeUserOptions: CatalogoOpcion[] = [];
+  statusUserOptions: CatalogoOpcion[] = [];
+  careersOptions: CatalogoOpcion[] = [];
 
   constructor(
     private cd: ChangeDetectorRef,
     private fb: FormBuilder,
     private usersService: UserService,
+    private catalogoOpcionService: OptionCatalogService,
+    private rolService: RolService,
   ) {}
 
   ngOnInit(): void {
@@ -72,13 +92,17 @@ export class DonationsManagmentComponent implements OnInit {
     return this.usersForm.invalid || (this.usersBDFormArray.length === 0 && this.newUsersFormArray.length === 0);
   }
 
+  get typeUser(): typeof TypeUserEnum {
+    return TypeUserEnum;
+  }
+
   addNewUser(): void {
     if (this.usersForm.get('typeUserAdd')?.value === this.USER_BD) {
-      return this.addExistUser();
+      return this.addExistUserForm();
     }
 
     if (this.usersForm.get('typeUserAdd')?.value === this.NEW_USER) {
-      // todo: por hacer
+      return this.addNewUserForm();
     }
   }
 
@@ -87,16 +111,78 @@ export class DonationsManagmentComponent implements OnInit {
     this.usersBDFormArray.controls.forEach(control => {
       control.get('id')?.updateValueAndValidity();
     });
-    this.cd.markForCheck();
   }
 
-  private addExistUser(): void {
+  removeNewUser(index: number): void {
+    this.newUsersFormArray.removeAt(index);
+  }
+
+  private addExistUserForm(): void {
     this.usersBDFormArray.push(this.fb.group({
-      id: [null, [Validators.required, uniqueUserValidator(() => this.usersBDFormArray)]]
+      id: [null, [Validators.required, uniqueUserValidator(() => this.usersBDFormArray)]],
+      donacion: this.fb.group({
+        referencia: ['El usuario ya estaba registrado en el club', [Validators.maxLength(500)]]
+      })
     }));
   }
 
+  private addNewUserForm(): void {
+    const newUserForm = this.fb.group({
+      nombreCompleto: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(191)
+        ]
+      ],
+      correo: ['', [Validators.required, Validators.email]],
+      telefono: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(10),
+          Validators.maxLength(10)
+        ]
+      ],
+      fechaNacimiento: ['', Validators.required],
+      sexoId: [null, Validators.required],
+      rolId: [null, Validators.required],
+      tipoId: [null, Validators.required],
+      donacion: this.fb.group({
+        referencia: [null, Validators.maxLength(500)]
+      })
+    });
+
+    this.newUsersFormArray.push(newUserForm);
+    newUserForm.get('tipoId')?.valueChanges
+      .pipe(
+        takeUntilDestroyed(this.destroyRef)
+      )
+      .subscribe(tipoId => {
+        generateValidatorsTypeUser(newUserForm, tipoId, this.fb);
+      });
+  }
+
   private getDataForm(): void {
-    this.usersService.findAll().subscribe(users => this.users = users);
+    forkJoin([
+      this.usersService.findAll(),
+      this.catalogoOpcionService.findByCatalogoId(CatalogoEnum.SEXO),
+      this.rolService.findAll(),
+      this.catalogoOpcionService.findByCatalogoId(CatalogoEnum.TIPO_ESCOLAR),
+      this.catalogoOpcionService.findByCatalogoId(CatalogoEnum.TURNO_ALUMNO),
+      this.catalogoOpcionService.findByCatalogoId(CatalogoEnum.TIPO_USUARIO),
+      this.catalogoOpcionService.findByCatalogoId(CatalogoEnum.ESTATUS_USUARIO),
+      this.catalogoOpcionService.findByCatalogoId(CatalogoEnum.CARRERAS_EDUCATIVAS),
+    ]).subscribe(response => {
+      this.users = response[0];
+      this.sexOptions = response[1];
+      this.rolesOptions = response[2];
+      this.typeEscolarOptions = response[3];
+      this.turnAlumnoOptions = response[4];
+      this.typeUserOptions = response[5];
+      this.statusUserOptions = response[6];
+      this.careersOptions = response[7];
+    });
   }
 }
--- src/app/dashboard/donations/pages/donations-managment/validators-donation-form.ts
+++ src/app/dashboard/donations/pages/donations-managment/validators-donation-form.ts
@@ -1,4 +1,13 @@
-import { AbstractControl, FormArray, ValidationErrors, ValidatorFn } from '@angular/forms';
+import {
+  AbstractControl,
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  ValidationErrors,
+  ValidatorFn,
+  Validators
+} from '@angular/forms';
+import { TypeUserEnum } from '@shared/enums/catalogo-opciones/type-user.enum';
 
 export const uniqueUserValidator = (usersArray: () => FormArray): ValidatorFn => {
   return (control: AbstractControl): ValidationErrors | null => {
@@ -19,4 +28,41 @@ export const uniqueUserValidator = (usersArray: () => FormArray): ValidatorFn =>
 
     return null;
   }
+}
+
+export const generateValidatorsTypeUser = (form: FormGroup, tipoId: any, fb: FormBuilder): void => {
+  form.removeControl('escolar');
+  form.removeControl('alumno');
+
+  if (tipoId === TypeUserEnum.ESCOLAR) {
+    form.addControl('escolar', fb.group({
+      matricula: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(20)
+        ]
+      ],
+      tipoId: [null, Validators.required]
+    }));
+
+    form.updateValueAndValidity();
+
+    return;
+  }
+
+  if (tipoId === TypeUserEnum.ALUMNO) {
+    form.addControl('alumno', fb.group({
+      semestre: [null, Validators.required],
+      carreraId: [null, Validators.required],
+      turnoId: [null, Validators.required],
+    }))
+
+    form.updateValueAndValidity();
+
+    return;
+  }
+
+  form.updateValueAndValidity();
 }
